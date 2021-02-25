b0609087bf953f3ea1d250e8dde9e23fa5cb7470
Evgeniy <bot.evgen2018@gmail.com>
2021-02-26 00:45:40 +0500

    slider_ready

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,23 @@
+import '../js/jquery-3.5.1.min'
 import '../sass/_style.sass'
 import '../styles/fonts.css'
+import '../styles/slick.css'
+import '../js/slick.min'
+
+$(document).ready(function () {
+   $('.carousel__inner').slick({
+      speed: 800,
+      prevArrow: '<button type="button" class="slick-prev"><img src="../assets/icons/left_solid.png"></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="../assets/icons/right_solid.png"></button>',
+      responsive: [
+         {
+            breakpoint: 991,
+            settings: {
+               dots: true,
+               arrows: false,
+            }
+         },
+      ]
+   });
+});
+
